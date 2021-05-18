f5ba2eb9f9c11b1dbd2a4bef8a7961035e9b86fe
Sung Lee <sunglee726@gmail.com>
2021-05-18 10:25:37 -0500

    Add project files.

---
+++ Cars/Controllers/CarsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cars.Controllers
+{
+    [ApiController]
+    [Route("Cars")]
+    public class CarsController : ControllerBase
+    {
+        private readonly string pathToFile;
+        private IEnumerable<Auto> autos;
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<CarsController> _logger;
+
+        public CarsController(ILogger<CarsController> logger, IWebHostEnvironment env)
+        {
+            _logger = logger;
+
+            pathToFile = env.ContentRootPath
+            + Path.DirectorySeparatorChar.ToString()
+            + "Data"
+            + Path.DirectorySeparatorChar.ToString()
+            + "honda_wmi.json";
+        }
+
+        private IEnumerable<Auto> GetDataSet()
+        {
+            string source = "";
+
+            using (StreamReader SourceReader = System.IO.File.OpenText(pathToFile))
+            {
+                source = SourceReader.ReadToEnd();
+            }
+
+            return JsonConvert.DeserializeObject<IEnumerable<Auto>>(source);
+        }
+
+        [HttpGet]
+        public IEnumerable<Auto> Get()
+        {
+            autos = GetDataSet();
+            return autos.ToArray();
+        }
+
+        [HttpGet("countries")]
+        public IEnumerable<string> GetCountries()
+        {
+            autos = GetDataSet();
+            var countries = autos.Select(x => x.Country).Distinct().ToArray();
+            return countries;
+        }
+    }
+}
---
+++ Cars/WMI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Auto
+    {
+        public string Country { get; set; }
+        public string CreatedOn { get; set; }
+        public string DateAvailableToPublic { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UpdatedOn { get; set; }
+        public string WMI { get; set; }
+    }
+}
